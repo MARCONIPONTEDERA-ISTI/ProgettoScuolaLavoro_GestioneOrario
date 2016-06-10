4f439db95f3d57b19aa18de39ebd3e00935558a0
Giorgio <imatesiu@gmail.com>
2016-06-10 15:53:22 +0200

    update settings

---
+++ GestioneOrario/app/src/main/java/marconi/isti/gestioneorario/ActivitySetting.java
@@ -0,0 +1,13 @@
+package marconi.isti.gestioneorario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_setting);
+    }
+}
