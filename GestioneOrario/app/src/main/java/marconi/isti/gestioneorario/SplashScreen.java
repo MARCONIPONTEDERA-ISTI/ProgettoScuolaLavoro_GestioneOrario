83884e2240713b77fe3893f1343d3f91055312f2
Giorgio <imatesiu@gmail.com>
2016-06-10 14:00:41 +0200

    plash screen added

---
+++ GestioneOrario/app/src/main/java/marconi/isti/gestioneorario/SplashScreen.java
@@ -0,0 +1,43 @@
+package marconi.isti.gestioneorario;
+
+/**
+ * Created by winspa on 10/06/2016.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Saeed on 9/12/2015.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
