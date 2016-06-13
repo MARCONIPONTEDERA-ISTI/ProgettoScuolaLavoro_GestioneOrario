b9a6c7b5d24125a128d1295989519c643462a49f
Giorgio <imatesiu@gmail.com>
2016-06-13 17:00:13 +0200

    zoom and scroll added

--- GestioneOrario/app/src/main/java/marconi/isti/gestioneorario/DataUIActivity.java
+++ GestioneOrario/app/src/main/java/marconi/isti/gestioneorario/DataUIActivity.java
@@ -1,24 +1,42 @@
 package marconi.isti.gestioneorario;
 
 import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+
+import android.graphics.Matrix.ScaleToFit;
 
 public class DataUIActivity extends AppCompatActivity {
 
     private ScaleGestureDetector scaleGestureDetector;
     private Matrix matrix = new Matrix();
-    private float scale = 5.0f;
+    private float scale = 10.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_ui);
 
-        scaleGestureDetector = new ScaleGestureDetector(this,new ScaleListener());
+       /* ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+
+        matrix = imageView.getImageMatrix();
+       long imageWidth = imageView.getDrawable().getIntrinsicWidth();
+        long imageHeight = 250;
+        RectF drawableRect = new RectF(0, 0, imageWidth, imageHeight);
+        RectF viewRect = new RectF(0, 0, imageView.getWidth(), imageView.getHeight());
+        matrix.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
+        imageView.setImageMatrix(matrix);
+
+
+
+
+
+        scaleGestureDetector = new ScaleGestureDetector(this,new ScaleListener());*/
     }
 
     @Override
@@ -27,14 +45,14 @@ public class DataUIActivity extends AppCompatActivity {
         return true;
     }
 
-    private class ScaleListener extends ScaleGestureDetector.
+  /*  private class ScaleListener extends ScaleGestureDetector.
             SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            float scaleFactor = detector.getScaleFactor();
+            //float scaleFactor = detector.getScaleFactor();
 
             scale *= detector.getScaleFactor();
-            scale = Math.max(5.0f, Math.min(scale, 25.0f));
+            //scale = Math.max(10.0f, Math.min(scale, 55.0f));
 
             matrix.setScale(scale, scale);
 
@@ -44,5 +62,5 @@ public class DataUIActivity extends AppCompatActivity {
             imageView.setImageMatrix(matrix);
             return true;
         }
-    }
+    }*/
 }
